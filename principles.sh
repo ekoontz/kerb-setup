323177fe2e930d7ef5e58ba17e694ab77e78027d
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-23 00:31:31 +0000

    adding argument-checking

--- principles.sh
+++ principles.sh
@@ -1,22 +1,44 @@
 #!/bin/sh
+
+PASSWORD=$1
+if [ -z $PASSWORD ]; then
+    echo "Usage: principles.sh <password>"
+    exit 1
+fi
+
 if [ -z $HOSTNAME ]; then
     HOSTNAME=`hostname -f`
 fi
-echo $HOSTNAME
+echo "using hostname: $HOSTNAME for server component of server principals."
 
-PASSWORD=$1
 KADMIN_LOCAL="sudo kadmin.local"
 NORMAL_USER=`whoami`
 
-#This script is idempotent: deletes existing principles and keytabs, if any,
-#before recreating.
+#This script is idempotent: running it multiple times: re-running it
+#results in the same state. Currently accomplished by deleting existing
+#principles and keytabs, if any, and then (re-)creating.
 
 #1. services
+SERVICE_KEYTAB=services.keytab
+
+rm -f `pwd`/$SERVICE_KEYTAB
+
+#zookeeper
 echo "delprinc -force zookeeper/$HOSTNAME" | $KADMIN_LOCAL
 echo "addprinc -randkey zookeeper/$HOSTNAME" | $KADMIN_LOCAL
-rm -f `pwd`/zookeeper.keytab
-echo "ktadd -k `pwd`/zookeeper.keytab zookeeper/$HOSTNAME" | $KADMIN_LOCAL
-sudo chown $NORMAL_USER `pwd`/zookeeper.keytab
+echo "ktadd -k `pwd`/$SERVICE_KEYTAB zookeeper/$HOSTNAME" | $KADMIN_LOCAL
+
+#hdfs
+echo "delprinc -force hdfs/$HOSTNAME" | $KADMIN_LOCAL
+echo "addprinc -randkey hdfs/$HOSTNAME" | $KADMIN_LOCAL
+echo "ktadd -k `pwd`/$SERVICE_KEYTAB hdfs/$HOSTNAME" | $KADMIN_LOCAL
+
+#host
+echo "delprinc -force host/$HOSTNAME" | $KADMIN_LOCAL
+echo "addprinc -randkey host/$HOSTNAME" | $KADMIN_LOCAL
+echo "ktadd -k `pwd`/$SERVICE_KEYTAB host/$HOSTNAME" | $KADMIN_LOCAL
+
+sudo chown $NORMAL_USER `pwd`/$SERVICE_KEYTAB
 
 #2. users
 echo "delprinc -force zkclient" | $KADMIN_LOCAL
@@ -24,6 +46,6 @@ echo "addprinc -pw $PASSWORD zkclient" | $KADMIN_LOCAL
 
 # only uncomment this if you want to use keytabs with client (rather
 # than password).
-rm -f `pwd`/zkclient.keytab
-echo "ktadd -k `pwd`/zkclient.keytab zkclient" | $KADMIN_LOCAL
-sudo chown $NORMAL_USER `pwd`/zkclient.keytab
+#rm -f `pwd`/zkclient.keytab
+#echo "ktadd -k `pwd`/zkclient.keytab zkclient" | $KADMIN_LOCAL
+#sudo chown $NORMAL_USER `pwd`/zkclient.keytab
