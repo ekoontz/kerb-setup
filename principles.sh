3e59abf1d77cf6ec83ce6839670e674097169ac5
Eugene Koontz <ekoontz@hiro-tan.org>
2012-06-23 01:22:24 +0000

    fix comment

--- principles.sh
+++ principles.sh
@@ -18,28 +18,28 @@ NORMAL_USER=`whoami`
 #as if you only ran it once. Currently accomplished by deleting existing
 #principles and keytabs, if any, and then (re-)creating.
 
-#host
+SERVICE_KEYTAB=services.keytab
+rm -f `pwd`/$SERVICE_KEYTAB
+
+
+#1. services
+
+#1.1. host
 echo "delprinc -force host/$HOSTNAME" | $KADMIN_LOCAL
 echo "addprinc -randkey host/$HOSTNAME" | $KADMIN_LOCAL
 echo "ktadd -k `pwd`/$SERVICE_KEYTAB host/$HOSTNAME" | $KADMIN_LOCAL
 
-#1. services
-SERVICE_KEYTAB=services.keytab
-
-rm -f `pwd`/$SERVICE_KEYTAB
-
-
-#1.1. zookeeper
+#1.2. zookeeper
 echo "delprinc -force zookeeper/$HOSTNAME" | $KADMIN_LOCAL
 echo "addprinc -randkey zookeeper/$HOSTNAME" | $KADMIN_LOCAL
 echo "ktadd -k `pwd`/$SERVICE_KEYTAB zookeeper/$HOSTNAME" | $KADMIN_LOCAL
 
-#1.2. hdfs
+#1.3. hdfs
 echo "delprinc -force hdfs/$HOSTNAME" | $KADMIN_LOCAL
 echo "addprinc -randkey hdfs/$HOSTNAME" | $KADMIN_LOCAL
 echo "ktadd -k `pwd`/$SERVICE_KEYTAB hdfs/$HOSTNAME" | $KADMIN_LOCAL
 
-#1.3. mapred
+#1.4. mapred
 echo "delprinc -force mapred/$HOSTNAME" | $KADMIN_LOCAL
 echo "addprinc -randkey mapred/$HOSTNAME" | $KADMIN_LOCAL
 echo "ktadd -k `pwd`/$SERVICE_KEYTAB mapred/$HOSTNAME" | $KADMIN_LOCAL
